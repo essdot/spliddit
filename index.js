cf5eb987e72e82ae0bbd278fe03622c39bb967d8
Justin Sippel <justin.sippel@urbanairship.com>
2015-09-18 14:22:21 -0700

    Fix regional indicator flags

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ module.exports = spliddit
 module.exports.isFirstOfPair = is_first_of_surrogate_pair
 module.exports.hasPair = has_pair
 
+REGIONAL_INDICATOR_START = 0x1F1E6
+REGIONAL_INDICATOR_END = 0x1F1FF
+
 function spliddit(s) {
   var i = 0
+    , next_two
     , returnArr = []
     , current
     , arr
@@ -23,6 +27,15 @@ function spliddit(s) {
 
     if(is_first_of_surrogate_pair(current)) {
       current += arr[i++]
+
+      if(is_regional_indicator_pair(current)) {
+        next_two = arr[i] + arr[i + 1]
+
+        if(is_regional_indicator_pair(next_two)) {
+          current += next_two
+          i += 2
+        }
+      }
     }
 
     returnArr.push(current)
@@ -49,4 +62,19 @@ function has_pair(s) {
   return arr.some(function(item) {
     return is_first_of_surrogate_pair(item)
   })
+}
+
+function code_point_from_surrogate_pair(first, second) {
+  return (first - 0xD800) * 0x400 + second - 0xDC00 + 0x10000
+}
+
+function is_regional_indicator_pair(pair) {
+  var code_point = code_point_from_surrogate_pair(
+    pair.charCodeAt(0), pair.charCodeAt(1)
+  )
+
+  return (
+    code_point >= REGIONAL_INDICATOR_START &&
+    code_point <= REGIONAL_INDICATOR_END
+  )
 }
